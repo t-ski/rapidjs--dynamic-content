7126152613e901d035025aaf16a565422f17b050
t-ski <t.schiepanski@gmx.de>
2021-03-17 01:15:09 +0100

    Create and set up module and init method of app

--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
 const config = {
-	webDirName: "web"
+    frontendModuleFileName: "rapid.loader.frontend.js"
 };
 
-let coreAppInstance;
+const {readFileSync} = require("fs");
+const {join} = require("path");
 
-server.finish("html", data => {
-    return data;
-});
+const FRONTEND_MODULE = readFileSync(join(__dirname, "frontend.js"));
 
-module.exports = coreApp => {
-    coreAppInstance = coreApp;
-};
+function init(coreAppInstance) {
+    // Add finisher
+    coreAppInstance.finish("", data => {
+        const headInsertionIndex = data.search(/<\s*\/head\s*>/);
+        if(headInsertionIndex == -1) {
+            return data;
+        }
+        data = data.slice(0, headInsertionIndex) + `<script src="/${config.frontendModuleFileName}"></script>` + data.slice(headInsertionIndex);
+        return data;
+    });
+
+    coreAppInstance.route("get", `/${config.frontendModuleFileName}`, _ => {
+        return FRONTEND_MODULE;
+    });
+}
+
+module.exports = init;
