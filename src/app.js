ec18061b2e0553aff6a305066459adc954f84c5c
t-ski <t.schiepanski@gmx.de>
2021-03-19 01:52:10 +0100

    Implement mechanism for inserting config object attrbiute values in frontend module data to prevent hardcoding

--- src/app.js
+++ src/app.js
@@ -5,7 +5,13 @@ const config = {
 const {readFileSync} = require("fs");
 const {join} = require("path");
 
-const FRONTEND_MODULE = readFileSync(join(__dirname, "frontend.js"));
+let frontendModuleData = String(readFileSync(join(__dirname, "frontend.js")));
+(frontendModuleData.match(/[^a-zA-Z0-9_]config\s*\.\s*[a-zA-Z0-9_]+/g) || []).forEach(configAttr => {
+    let value = config[configAttr.match(/[a-zA-Z0-9_]+$/)[0]] || "null";
+    isNaN(value) && (value = `"${value}"`);
+
+    frontendModuleData = frontendModuleData.replace(configAttr, `${configAttr.slice(0, 1)}${value}`);
+});
 
 function init(coreAppInstance) {
     // Add finisher
@@ -17,9 +23,11 @@ function init(coreAppInstance) {
         data = data.slice(0, headInsertionIndex) + `<script src="/${config.frontendModuleFileName}"></script>` + data.slice(headInsertionIndex);
         return data;
     });
+    
+    coreAppInstance.route("get", `/${config.frontendModuleFileName}`, res => {
+        res.setHeader("Content-Type", "text/javascript");
 
-    coreAppInstance.route("get", `/${config.frontendModuleFileName}`, _ => {
-        return FRONTEND_MODULE;
+        return frontendModuleData;
     });
 }
 
--- src/frontend.js
+++ src/frontend.js
@@ -1,6 +1,24 @@
 const RAPID = (module => {
 
-    // ...
+    function post(url, body) {
+        return fetch(url, {
+            method: "POST",
+            mode: "same-origin",
+            credentials: "same-origin",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            redirect: "follow",
+            referrerPolicy: "no-referrer",
+            body: JSON.stringify(body)
+        });
+    }
+
+    HTMLElement.prototype.load = function(id) {
+        let data = post();
+        console.log(data);
+    };
 
     return module;
+
 })(RAPID || {});
