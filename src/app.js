04f247523897663b792cbf4bf233c05bf5b172ce
t-ski <t.schiepanski@gmx.de>
2021-03-22 18:11:53 +0100

    Complete loading method implementation and set up content loaded event mechanism

--- src/app.js
+++ src/app.js
@@ -1,22 +1,34 @@
 const config = {
+    defaultContentName: "index",
+    dynamicPageDirPrefix: ":",
     frontendModuleFileName: "rapid.loader.frontend.js",
-    requestEndpoint: "_load"
+    loadEventName: "rapid--ContentLoaded",
+    requestEndpoint: "_load",
+    wrapperElementAttribute: "rapid--wrapper"
 };
 
-const {readFileSync} = require("fs");
+const {readFileSync, existsSync} = require("fs");
 const {join} = require("path");
 
+// Substitute config attribute usages in frontend module to be able to use the same config object between back- and frontend
 let frontendModuleData = String(readFileSync(join(__dirname, "frontend.js")));
 (frontendModuleData.match(/[^a-zA-Z0-9_]config\s*\.\s*[a-zA-Z0-9_]+/g) || []).forEach(configAttr => {
-    let value = config[configAttr.match(/[a-zA-Z0-9_]+$/)[0]] || "null";
-    isNaN(value) && (value = `"${value}"`);
-
+    let value = config[configAttr.match(/[a-zA-Z0-9_]+$/)[0]];
+    (value !== undefined && value !== null && isNaN(value)) && (value = `"${value}"`);
+    
     frontendModuleData = frontendModuleData.replace(configAttr, `${configAttr.slice(0, 1)}${value}`);
 });
 
 function init(coreAppInstance) {
     // Add finisher
     coreAppInstance.finish("", data => {
+        // No further action if no wrapper element attribute found in data string
+        // (ignoring non-attribute use; double checked in frontend)
+        if(!data.includes(config.wrapperElementAttribute)) {
+            return data;
+        }
+
+        // Insert frontend module loading script tag
         const headInsertionIndex = data.search(/<\s*\/head\s*>/);
         if(headInsertionIndex == -1) {
             return data;
@@ -25,15 +37,25 @@ function init(coreAppInstance) {
         return data;
     });
     
+    // Add GET route to retrieve frontend module script
     coreAppInstance.route("get", `/${config.frontendModuleFileName}`, res => {
         res.setHeader("Content-Type", "text/javascript");
 
         return frontendModuleData;
     });
 
+    // Add POST route to retrieve specific content
     coreAppInstance.route("post", `/${config.requestEndpoint}`, body => {
-        console.log(body);
-        return "...";
+        if(!body.content) {
+            body.content = config.defaultContentName;
+        }
+
+        const contentFilePath = join(coreAppInstance.getWebPath(), body.pathname, `:${body.content}.html`);
+        if(!existsSync(contentFilePath)) {
+            throw 404;
+        }
+
+        return String(readFileSync(contentFilePath));
     });
 }
 
--- src/frontend.js
+++ src/frontend.js
@@ -1,4 +1,29 @@
+"use strict";
+
 var RAPID = (module => {
+
+    // Reusable runtime runtimeData storage
+    let runtimeData = {};
+
+    const CONTENT_NAME_REGEX = new RegExp(`(\\${config.dynamicPageDirPrefix}[a-z0-9_-]+)+`, "i");
+
+    // Initialize
+    document.addEventListener("DOMContentLoaded", _ => {
+        // Retrieve wrapper element instance
+        runtimeData.wrapper = document.querySelector(`*[${config.wrapperElementAttribute}]`);
+        if(!runtimeData.wrapper) {
+            // No further action as no wrapper element defined
+            return;
+        }
+
+        runtimeData.wrapper.removeAttribute(config.wrapperElementAttribute);
+        
+        runtimeData.contentName = document.location.pathname.match(CONTENT_NAME_REGEX);
+        runtimeData.contentName && (runtimeData.contentName = runtimeData.contentName[0].split(config.dynamicPageDirPrefix)[1]);
+        !runtimeData.contentName && (runtimeData.contentName = config.defaultContentName);
+
+        load(runtimeData.contentName);
+    });
 
     function post(url, body) {
         return fetch(url, {
@@ -15,19 +40,47 @@ var RAPID = (module => {
     }
 
     /**
-     * Load markup into a given host element.
-     * @param {HTMLElement} element Host element
+     * Load markup into the designated wrapper element.
      * @param {String} content Content name
      */
-    module.load = function(element, content) {
+    function load(content) {
+        if(!runtimeData.wrapper) {
+            return;
+        }
+
+        const baseIndex = document.location.pathname.lastIndexOf("/") + 1;
+        const internalPathname = `${document.location.pathname.slice(0, baseIndex)}${config.dynamicPageDirPrefix}${document.location.pathname.slice(baseIndex).replace(CONTENT_NAME_REGEX, ``)}`;
         post(config.requestEndpoint, {
-            pathname: document.location.pathname,
+            pathname: internalPathname,
             content: content
-        }).then(data => data.json()).then(data => {
-            element.innerHTML = data
+        }).then(data => {
+            if(data.status != 200) {
+                throw 404;
+            }
+            
+            return data.json();
+        }).then(data => {
+            runtimeData.wrapper.innerHTML = String(data);
+
+            // Dispatch content loaded event
+            const event = new Event(config.loadEventName);
+            document.dispatchEvent(event);
+        }).catch(err => {
+            document.location.href = document.location.pathname.replace(/\/[^\/]+$/, "/404.html");
         });
     };
 
-    return module;
+    // INTERFACE
 
+    module.load = function(content) {
+        load(content);
+        
+        const newPathname = document.location.href.replace(CONTENT_NAME_REGEX, `${config.dynamicPageDirPrefix}${content}`);
+        history.pushState({
+
+        }, "", newPathname); // TODO: (How to) send body along?
+    };
+
+    return module;
+    
 })(RAPID || {});
