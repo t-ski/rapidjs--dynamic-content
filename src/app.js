43f3fc1d56a9260a84d46366b95802e454441ff9
t-ski <t.schiepanski@gmx.de>
2021-03-24 01:40:52 +0100

    Add history manipulation from dynamic load calls

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const config = {
-    defaultContentName: "index",
-    dynamicPageDirPrefix: ":",
-    frontendModuleFileName: "rapid.loader.frontend.js",
-    loadEventName: "rapid--ContentLoaded",
-    requestEndpoint: "_load",
-    wrapperElementAttribute: "rapid--wrapper"
+	defaultContentName: "index",
+	dynamicPageDirPrefix: ":",
+	frontendModuleFileName: "rapid.loader.frontend.js",
+	loadEventName: "rapid--ContentLoaded",
+	requestEndpoint: "_load",
+	wrapperElementAttribute: "rapid--wrapper"
 };
 
 const {readFileSync, existsSync} = require("fs");
@@ -13,50 +13,50 @@ const {join} = require("path");
 // Substitute config attribute usages in frontend module to be able to use the same config object between back- and frontend
 let frontendModuleData = String(readFileSync(join(__dirname, "frontend.js")));
 (frontendModuleData.match(/[^a-zA-Z0-9_]config\s*\.\s*[a-zA-Z0-9_]+/g) || []).forEach(configAttr => {
-    let value = config[configAttr.match(/[a-zA-Z0-9_]+$/)[0]];
-    (value !== undefined && value !== null && isNaN(value)) && (value = `"${value}"`);
+	let value = config[configAttr.match(/[a-zA-Z0-9_]+$/)[0]];
+	(value !== undefined && value !== null && isNaN(value)) && (value = `"${value}"`);
     
-    frontendModuleData = frontendModuleData.replace(configAttr, `${configAttr.slice(0, 1)}${value}`);
+	frontendModuleData = frontendModuleData.replace(configAttr, `${configAttr.slice(0, 1)}${value}`);
 });
 
 function init(coreAppInstance) {
-    // Add finisher
-    coreAppInstance.finish("", data => {
-        // No further action if no wrapper element attribute found in data string
-        // (ignoring non-attribute use; double checked in frontend)
-        if(!data.includes(config.wrapperElementAttribute)) {
-            return data;
-        }
+	// Add finisher
+	coreAppInstance.finish("", data => {
+		// No further action if no wrapper element attribute found in data string
+		// (ignoring non-attribute use; double checked in frontend)
+		if(!data.includes(config.wrapperElementAttribute)) {
+			return data;
+		}
 
-        // Insert frontend module loading script tag
-        const headInsertionIndex = data.search(/<\s*\/head\s*>/);
-        if(headInsertionIndex == -1) {
-            return data;
-        }
-        data = data.slice(0, headInsertionIndex) + `<script src="/${config.frontendModuleFileName}"></script>` + data.slice(headInsertionIndex);
-        return data;
-    });
+		// Insert frontend module loading script tag
+		const headInsertionIndex = data.search(/<\s*\/head\s*>/);
+		if(headInsertionIndex == -1) {
+			return data;
+		}
+		data = data.slice(0, headInsertionIndex) + `<script src="/${config.frontendModuleFileName}"></script>` + data.slice(headInsertionIndex);
+		return data;
+	});
     
-    // Add GET route to retrieve frontend module script
-    coreAppInstance.route("get", `/${config.frontendModuleFileName}`, res => {
-        res.setHeader("Content-Type", "text/javascript");
+	// Add GET route to retrieve frontend module script
+	coreAppInstance.route("get", `/${config.frontendModuleFileName}`, res => {
+		res.setHeader("Content-Type", "text/javascript");
 
-        return frontendModuleData;
-    });
+		return frontendModuleData;
+	});
 
-    // Add POST route to retrieve specific content
-    coreAppInstance.route("post", `/${config.requestEndpoint}`, body => {
-        if(!body.content) {
-            body.content = config.defaultContentName;
-        }
+	// Add POST route to retrieve specific content
+	coreAppInstance.route("post", `/${config.requestEndpoint}`, body => {
+		if(!body.content) {
+			body.content = config.defaultContentName;
+		}
 
-        const contentFilePath = join(coreAppInstance.getWebPath(), body.pathname, `:${body.content}.html`);
-        if(!existsSync(contentFilePath)) {
-            throw 404;
-        }
+		const contentFilePath = join(coreAppInstance.getWebPath(), body.pathname, `:${body.content}.html`);
+		if(!existsSync(contentFilePath)) {
+			throw 404;
+		}
 
-        return String(readFileSync(contentFilePath));
-    });
+		return String(readFileSync(contentFilePath));
+	});
 }
 
 module.exports = init;
--- src/frontend.js
+++ src/frontend.js
@@ -1,94 +1,138 @@
+// ESLint directives
+/*global config*/
+
 "use strict";
 
 var RAPID = (module => {
 
-    // Reusable runtime runtimeData storage
-    let runtimeData = {};
+	// Reusable runtime runtimeData storage
+	let runtimeData = {};
 
-    const CONTENT_NAME_REGEX = new RegExp(`(\\${config.dynamicPageDirPrefix}[a-z0-9_-]+)+`, "i");
+	const CONTENT_NAME_REGEX = new RegExp(`(\\${config.dynamicPageDirPrefix}[a-z0-9_-]+)+`, "i");
 
-    // Initialize
-    document.addEventListener("DOMContentLoaded", _ => {
-        // Retrieve wrapper element instance
-        runtimeData.wrapper = document.querySelector(`*[${config.wrapperElementAttribute}]`);
-        if(!runtimeData.wrapper) {
-            // No further action as no wrapper element defined
-            return;
-        }
+	// Initialize
+	document.addEventListener("DOMContentLoaded", _ => {
+		// Retrieve wrapper element instance
+		runtimeData.wrapper = document.querySelector(`*[${config.wrapperElementAttribute}]`);
+		if(!runtimeData.wrapper) {
+			// No further action as no wrapper element defined
+			return;
+		}
 
-        runtimeData.wrapper.removeAttribute(config.wrapperElementAttribute);
+		runtimeData.wrapper.removeAttribute(config.wrapperElementAttribute);
         
-        runtimeData.contentName = document.location.pathname.match(CONTENT_NAME_REGEX);
-        runtimeData.contentName && (runtimeData.contentName = runtimeData.contentName[0].split(config.dynamicPageDirPrefix)[1]);
-        !runtimeData.contentName && (runtimeData.contentName = config.defaultContentName);
+		runtimeData.contentName = document.location.pathname.match(CONTENT_NAME_REGEX);
+		runtimeData.contentName && (runtimeData.contentName = runtimeData.contentName[0].split(config.dynamicPageDirPrefix)[1]);
+		!runtimeData.contentName && (runtimeData.contentName = config.defaultContentName);
 
-        load(runtimeData.contentName);
-    });
+		load(runtimeData.contentName);
 
-    function post(url, body) {
-        return fetch(url, {
-            method: "POST",
-            mode: "same-origin",
-            credentials: "same-origin",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            redirect: "follow",
-            referrerPolicy: "no-referrer",
-            body: JSON.stringify(body)
-        });
+        history.replaceState(getStateObj(), "");
+	});
+    // Intercept backwards navigation to handle it accordingly
+    window.addEventListener("popstate", e => {
+		if(!e.state) {
+			return;
+		}
+        
+        e.preventDefault();
+		load(e.state.content);
+	});
+
+	function post(url, body) {
+		return fetch(url, {
+			method: "POST",
+			mode: "same-origin",
+			credentials: "same-origin",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			redirect: "follow",
+			referrerPolicy: "no-referrer",
+			body: JSON.stringify(body)
+		});
+	}
+
+    function getStateObj() {
+        return {
+            content: runtimeData.contentName
+        };
     }
 
-    /**
+	/**
      * Load markup into the designated wrapper element.
      * @param {String} content Content name
+     * @param {Function} [progressCallback] Callback getting passed a content download progress value [0, 1] for custom loading time handling (e.g. visual feedback)
      */
-    function load(content) {
-        if(!runtimeData.wrapper) {
-            return;
-        }
+	function load(content, progressCallback) {
+		if(!runtimeData.wrapper) {
+			return;
+		}
 
-        const baseIndex = document.location.pathname.lastIndexOf("/") + 1;
-        const internalPathname = `${document.location.pathname.slice(0, baseIndex)}${config.dynamicPageDirPrefix}${document.location.pathname.slice(baseIndex).replace(CONTENT_NAME_REGEX, ``)}`;
-        post(config.requestEndpoint, {
-            pathname: internalPathname,
-            content: content || config.defaultContentName
-        }).then(data => {
-            if(data.status != 200) {
-                throw 404;
+		const baseIndex = document.location.pathname.lastIndexOf("/") + 1;
+		const internalPathname = `${document.location.pathname.slice(0, baseIndex)}${config.dynamicPageDirPrefix}${document.location.pathname.slice(baseIndex).replace(CONTENT_NAME_REGEX, "")}`;
+		post(config.requestEndpoint, {
+			pathname: internalPathname,
+			content: content || config.defaultContentName
+		}).then(async res => {
+			if(res.status != 200) {
+				document.location.href = document.location.pathname.replace(/\/[^/]+$/, "/404.html");
+			}
+            
+            // Explicitly download body to handle progress
+            const contentLength = res.headers.get("Content-Length");;
+            let receivedLength = 0;
+
+            const reader = res.body.getReader();
+            let chunks = [];
+            let curChunk;
+            while((curChunk = await reader.read()) && !curChunk.done) {
+                callProgressCallback(receivedLength / contentLength);
+
+                receivedLength += curChunk.value.length;
+                chunks.push(curChunk.value);
+            }
+            callProgressCallback(1);
+
+            let chunksAll = new Uint8Array(receivedLength);
+            let position = 0;
+            for(let chunk of chunks) {
+                chunksAll.set(chunk, position);
+                position += chunk.length;
             }
             
-            return data.json();
-        }).then(data => {
-            runtimeData.wrapper.innerHTML = String(data);
+			runtimeData.wrapper.innerHTML = JSON.parse(new TextDecoder("utf-8").decode(chunksAll));
 
-            // Dispatch content loaded event
-            const event = new Event(config.loadEventName);
-            document.dispatchEvent(event);
-        }).catch(err => {
-            document.location.href = document.location.pathname.replace(/\/[^\/]+$/, "/404.html");
-        });
-    };
+			// Dispatch content loaded event
+			const event = new Event(config.loadEventName);
+			document.dispatchEvent(event);
+		});
 
-    // INTERFACE
+        function callProgressCallback(progress) {
+            progressCallback && progressCallback(progress);
+        }
+	}
 
-    module.load = function(content) {
-        load(content);  // TODO: (How to) send body along?
+	// INTERFACE
+
+	module.load = function(content, progressCallback) {
+		load(content, progressCallback);  // TODO: (How to) send body along?
         
-        const newPathname = document.location.href.replace(CONTENT_NAME_REGEX, `${config.dynamicPageDirPrefix}${content}`);
-        history.pushState({
+        runtimeData.contentName = content;
 
-        }, "", newPathname);
-    };
+        // Manipulate history object
+		const newPathname = document.location.pathname.replace(CONTENT_NAME_REGEX, "") + ((content == config.defaultContentName) ? "" : (config.dynamicPageDirPrefix + content));
+		history.pushState(getStateObj(), "", newPathname);
+	};
 
-    /**
+	/**
      * Get the name of the currently loaded content.
      * @returns {String} Content name
      */
-    module.contentName = function() {
-        return runtimeData.contentName;
-    };
+	module.contentName = function() {
+		return runtimeData.contentName;
+	};
 
-    return module;
+	return module;
     
 })(RAPID || {});
